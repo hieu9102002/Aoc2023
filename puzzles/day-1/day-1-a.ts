99366a436500c358dfcc48807ef26704087b41ba
Martin Vu <hieu.vu@pointsbet.com>
2023-12-04 12:50:42 +1100

    day 1

--- puzzles/day-1/day-1-a.ts
+++ puzzles/day-1/day-1-a.ts
@@ -3,7 +3,10 @@ import chalk from 'chalk';
 
 export async function day1a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  const result = data.map(line => [...line.matchAll(/[0-9]/g)])
+                      .map(arr=>Number.parseInt(arr.at(0).toString())*10+Number.parseInt(arr.at(-1).toString()))
+                      .reduce((prev, cur)=>prev+cur)
+  return result;
 }
 
 const answer = await day1a();
--- puzzles/day-1/day-1-b.ts
+++ puzzles/day-1/day-1-b.ts
@@ -1,9 +1,32 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const digitMap = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+  zero: 0
+}
+
+const reverseMap = Object.fromEntries(Object.keys(digitMap).map(digit=>[digit.split("").reverse().join(""), digitMap[digit]]))
+
+const regex = new RegExp(`[0-9]|${Object.keys(digitMap).join('|')}`)
+const reverseRegex = new RegExp(`[0-9]|${Object.keys(reverseMap).join('|')}`)
+
+const mapDigitToNum = (x: string): number => digitMap[x] ?? reverseMap[x] ?? Number.parseInt(x);
+
 export async function day1b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  const result = data.map(line => [line.match(regex).toString(), line.split("").reverse().join("").match(reverseRegex).toString()])
+                      .map(([a,b]) => mapDigitToNum(a)*10+mapDigitToNum(b))
+                      .reduce((prev, cur) => prev+cur)
+  return result;
 }
 
 const answer = await day1b();
