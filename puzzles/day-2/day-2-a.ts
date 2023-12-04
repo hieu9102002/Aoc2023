097d44a958f6bb9b69c0623b05a8ecb4fcdf016c
Martin Vu <hieu.vu@pointsbet.com>
2023-12-04 22:09:01 +1100

    day 2 solution

--- puzzles/day-2/day-2-a.ts
+++ puzzles/day-2/day-2-a.ts
@@ -1,9 +1,32 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+type Color = 'blue' | 'red' | 'green'
+type Bag = Record<Color, number|undefined>
 export async function day2a(dataPath?: string) {
+  const initial: Bag = {
+    blue: 14,
+    red: 12,
+    green: 13
+  }
   const data = await readData(dataPath);
-  return 0;
+  const result = data
+    .map(line => {
+      const [,data] = line.split(":")
+      const pulls = data.split(";")
+                        .map<Bag>(pull=>{
+                          const colorPairs = pull.split(',')
+                                                  .map<[Color, number]>(pair => {
+                                                    const [no, col] = pair.trim().split(' ')
+                                                    return [col as Color, Number.parseInt(no)]
+                                                  })
+                          return Object.fromEntries(colorPairs) as Bag
+                        })
+      return pulls.every(pull=>Object.keys(initial).every(color => initial[color] >= (pull[color] ?? 0)))
+    })
+    .reduce((prev, cur, i) => cur ? prev + i + 1 : prev, 0)
+    
+  return result;
 }
 
 const answer = await day2a();
--- puzzles/day-2/day-2-b.ts
+++ puzzles/day-2/day-2-b.ts
@@ -1,9 +1,33 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+type Color = 'blue' | 'red' | 'green'
+type Bag = Record<Color, number | undefined>
 export async function day2b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  const result = data
+    .map(line => {
+      const [,data] = line.split(":")
+      const {blue, red, green} = data.split(";")
+                        .map<Bag>(pull=>{
+                          const colorPairs = pull.split(',')
+                                                  .map<[Color, number]>(pair => {
+                                                    const [no, col] = pair.trim().split(' ')
+                                                    return [col as Color, Number.parseInt(no)]
+                                                  })
+                          return Object.fromEntries(colorPairs) as Bag
+                        })
+                        .reduce<Bag>((prev, cur) => {
+                          return {
+                            blue: prev.blue < (cur.blue ?? 0) ? cur.blue : prev.blue,
+                            red: prev.red < (cur.red ?? 0) ? cur.red : prev.red,
+                            green: prev.green < (cur.green ?? 0) ? cur.green : prev.green
+                          }
+                        }, {blue: 0, red: 0, green: 0})
+      return blue*red*green
+    }).reduce((prev, cur)=>prev+cur)
+    
+  return result;
 }
 
 const answer = await day2b();
