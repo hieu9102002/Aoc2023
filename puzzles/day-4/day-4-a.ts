1a258c602eb1f326020847ec8ce6d75f65efccba
Martin Vu <hieu.vu@pointsbet.com>
2023-12-05 13:44:09 +1100

    day 4

--- puzzles/day-4/day-4-a.ts
+++ puzzles/day-4/day-4-a.ts
@@ -3,7 +3,15 @@ import chalk from 'chalk';
 
 export async function day4a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  const result = data.map(line => {
+    const [,input] = line.split(':')
+    const [winning, mine] = input.split('|')
+    const winningNumbers = [...winning.matchAll(/[0-9]+/g)].map(([val])=>Number.parseInt(val))
+    const myNumbers = [...mine.matchAll(/[0-9]+/g)].map(([val])=>Number.parseInt(val))
+    const matches = myNumbers.reduce((prev, cur) => winningNumbers.includes(cur) ? prev + 1 : prev, 0)
+    return matches === 0 ? 0 : Math.pow(2, matches - 1)
+  }).reduce((prev,cur)=> prev+cur)
+  return result;
 }
 
 const answer = await day4a();
--- puzzles/day-4/day-4-b.ts
+++ puzzles/day-4/day-4-b.ts
@@ -3,7 +3,25 @@ import chalk from 'chalk';
 
 export async function day4b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  const winningMap: Record<number, number> = Object.fromEntries(
+    data.map((line, i) => {
+      const [,input] = line.split(':')
+      const [winning, mine] = input.split('|')
+      const winningNumbers = [...winning.matchAll(/[0-9]+/g)].map(([val])=>Number.parseInt(val))
+      const myNumbers = [...mine.matchAll(/[0-9]+/g)].map(([val])=>Number.parseInt(val))
+      const matches = myNumbers.reduce((prev, cur) => winningNumbers.includes(cur) ? prev + 1 : prev, 0)
+      return [i, matches]
+    })
+  )
+  const cards: Record<number, number> = Object.fromEntries(Object.keys(winningMap).map(key=>[key,1]));
+  Object.entries(winningMap).forEach(([i, no])=>{
+    const noTickets = cards[i]
+    for (let j = 1; j <= no; j++){
+      cards[Number.parseInt(i)+j] += noTickets;
+    }
+  })
+  const result = Object.values(cards).reduce((prev,cur)=>prev+cur)
+  return result;
 }
 
 const answer = await day4b();
