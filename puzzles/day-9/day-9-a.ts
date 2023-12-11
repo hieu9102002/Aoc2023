c27d4be70466cfe3c11a4323e839f993a6e65893
Martin Vu <hieu.vu@pointsbet.com>
2023-12-11 11:04:15 +1100

    day 9

--- puzzles/day-9/day-9-a.ts
+++ puzzles/day-9/day-9-a.ts
@@ -3,7 +3,24 @@ import chalk from 'chalk';
 
 export async function day9a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  const result = data.map(line=>line.trim().split(' ').map(n=>Number.parseInt(n)))
+    .map(predictNext)
+    .reduce((prev, cur)=>prev+cur)
+  return result;
+}
+
+function predictNext(arr: number[]): number {
+  const gap:number[] = []
+
+  for (var i = 1; i < arr.length; i++)
+    gap.push(arr[i]-arr[i-1])
+
+  if (gap.every(x=>x===0)){
+    return arr.at(-1)
+  }
+
+
+  return arr.at(-1) + predictNext(gap)
 }
 
 const answer = await day9a();
--- puzzles/day-9/day-9-b.ts
+++ puzzles/day-9/day-9-b.ts
@@ -3,7 +3,24 @@ import chalk from 'chalk';
 
 export async function day9b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  const result = data.map(line=>line.trim().split(' ').map(n=>Number.parseInt(n)))
+    .map(numbers=>predictNext(numbers, true))
+    .reduce((prev, cur)=>prev+cur)
+  return result;
+}
+
+function predictNext(arr: number[], prev: boolean): number {
+  const gap:number[] = []
+
+  for (var i = 1; i < arr.length; i++)
+    gap.push(arr[i]-arr[i-1])
+
+  if (gap.every(x=>x===0)){
+    return arr.at(prev?0:-1)
+  }
+
+
+  return arr.at(prev?0:-1) + (prev?-1:1) * predictNext(gap, prev)
 }
 
 const answer = await day9b();
